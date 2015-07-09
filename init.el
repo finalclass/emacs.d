1aee3c2e581f9594aa51fb9103e4d1ed2831f6dd
Szymon Wygnanski <szymon@spotme.com>
2015-07-09 12:13:49 +0200

    Kill and join forward added

--- init.el
+++ init.el
@@ -57,9 +57,13 @@
                  (let ((mark-even-if-inactive transient-mark-mode))
                    (indent-region (region-beginning) (region-end) nil))))))
 
+					;kill and join forward
 
-
-
+(defadvice kill-line (before check-position activate)
+  (if (and (eolp) (not (bolp)))
+      (progn (forward-char 1)
+             (just-one-space 0)
+             (backward-char 1))))
 
 					;shell
 
