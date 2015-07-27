c83f44f4ba416b345d33b889eebbcc52e4af8979
Szymon Wygnanski <s@finalclass.net>
2015-07-28 00:41:41 +0200

    Adding dired sorting

--- init.el
+++ init.el
@@ -26,6 +26,21 @@
 
 (init--install-packages)
 
+                                        ;Dire
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header 
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+    (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
+
 					;setup extensions
 (require 'setup-yasnippet)
 
