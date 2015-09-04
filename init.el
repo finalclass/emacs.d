015117503ec9c1f2af0606fb1d3d0aab7edb3246
Szymon Wygnanski <szymon@spotme.com>
2015-09-04 10:26:25 +0200

    added webmode

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
   (package-install 'auto-complete)
   (package-install 'darcula-theme)
   (package-install 'emmet-mode)
+  (package-install 'web-mode)
   (package-install 'company-restclient)
   (package-install 'ac-js2)
   (package-install 'yasnippet)
@@ -25,6 +26,21 @@
   )
 
 (init--install-packages)
+
+                                        ;web mode
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-enable-current-element-highlight t)
+(setq web-mode-enable-current-column-highlight t)
 
                                         ;Dire
 (defun mydired-sort ()
