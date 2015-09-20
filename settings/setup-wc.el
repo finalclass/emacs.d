5678da40d1deb310c38391f654e2b9a11def4f39
Szymon Wygnanski <s@finalclass.net>
2015-09-20 03:26:02 +0200

    wc no change without configuramtion

--- settings/setup-wc.el
+++ settings/setup-wc.el
@@ -9,16 +9,20 @@
 (defun wc-save ()
   "Saves current window configuration to the end of the 'wc-list"
   (interactive)
-  (add-to-list 'wc-list (current-window-configuration) t)
-  (setq wc-position (- (wc-list-length) 1))
-  (message (concat "Window Configuration Saved @ " (number-to-string wc-position))))
+  (setcar (nthcdr wc-position wc-list) (current-window-configuration))
+  ;;(add-to-list 'wc-list (current-window-configuration) t)
+  ;;(setq wc-position (- wc-position 1))
+  (message "set conf %s" wc-position))
 
 (defun wc-remove ()
   "Removes one window configuration at current wc-position"
   (interactive)
-  (setq wc-list (delete (nth wc-position wc-list) wc-list))
-  (message (concat "Window Configuration Removed @ " (number-to-string wc-position)))
-  (wc-dec))
+  (wc-set-to-current)
+  (message "open conf %s" wc-position)
+  ;(setq wc-list (delete (nth wc-position wc-list) wc-list))
+  ;(message (concat "Window Configuration Removed @ " (number-to-string wc-position)))
+;  (wc-dec)
+  )
 
 (defun wc-inc ()
   "Increases wc-position"
@@ -30,7 +34,7 @@
 (defun wc-set-to-current ()
   "Sets window-configuration to current (by wc-position) element from wc-list list"
   (interactive)
-  (message "Current position %s" wc-position)
+  (message "loading %s" wc-position)
   (if (nth wc-position wc-list)
       (set-window-configuration (nth wc-position wc-list))))
 
@@ -38,7 +42,9 @@
   "Sets window-configuration to the next element of wc-list"
   (interactive)
   (wc-inc)
-  (wc-set-to-current))
+  ;;(wc-set-to-current)
+  (message "conf %s" wc-position)
+  )
 
 (defun wc-dec ()
   "Decreases wc-position (position of the wc-list list)"
@@ -51,6 +57,21 @@
   "Sets window-configuration to the previous element of wc-list current position element"
   (interactive)
   (wc-dec)
-  (wc-set-to-current))
+  (message "conf %s" wc-position)
+  ;(wc-set-to-current)
+  )
 
 (provide 'setup-wc)
+
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+(add-to-list 'wc-list (current-window-configuration) t)
+
+
