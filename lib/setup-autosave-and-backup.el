f6e2b661a03a01ff555946d18479d84db3251521
Szymon WYgnanski <s@finalclass.net>
2016-02-23 11:08:42 +0100

    Disabling interlocks (.#* files)

--- lib/setup-autosave-and-backup.el
+++ lib/setup-autosave-and-backup.el
@@ -1,9 +1,10 @@
-
 					;autosave & backup
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+                                        ;disable interlocks (.#* files)
+(setq create-lockfiles nil)
 
 (provide 'setup-autosave-and-backup)
